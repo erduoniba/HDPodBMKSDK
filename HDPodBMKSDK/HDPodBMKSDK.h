d2b4c099264dcb900830d23b9ae24e6bc7904de9
harrydeng <13049862397@163.com>
2017-05-03 23:42:26 +0800

    Initial Commit

---
+++ HDPodBMKSDK/HDPodBMKSDK.h
@@ -0,0 +1,19 @@
+//
+//  HDPodBMKSDK.h
+//  HDPodBMKSDK
+//
+//  Created by 邓立兵 on 2017/5/3.
+//  Copyright © 2017年 邓立兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HDPodBMKSDK.
+FOUNDATION_EXPORT double HDPodBMKSDKVersionNumber;
+
+//! Project version string for HDPodBMKSDK.
+FOUNDATION_EXPORT const unsigned char HDPodBMKSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDPodBMKSDK/PublicHeader.h>
+
+
